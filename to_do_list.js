3ca0fd0c2c10d47f5e7096021d2ed7f3f2ce3be8
Jones3bc <Jones3bc@cmich.edu>
2023-09-26 11:52:52 -0400

    Initial to_do_list html and JS implementations.
    Added an add button that works at a simple level.
    
    Added a remove button for each entry that removes list items.

--- to_do_list.js
+++ to_do_list.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const $ = selector => document.querySelector(selector);
+
+var list = $("#taskList");
+var id = 1;
+
+function addItem(){
+    let entry = document.createElement('li');
+    entry.appendChild(document.createTextNode("Example Task #" + id++ + " Due Date: 10/23/2023"));
+    let deleteButton = document.createElement("button");
+    //add a click event to remove the current item
+    deleteButton.addEventListener("click", function() {
+      entry.remove()
+    });
+    deleteButton.textContent = "X";
+    entry.appendChild(deleteButton);
+    list.appendChild(entry);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	$("#add").addEventListener("click", addItem);
+});
