735bc3b43743de85fcf420b55c0b354dc59e3456
Jones3bc <Jones3bc@cmich.edu>
2023-09-28 10:10:31 -0400

    Added more implementation for to_do_list.js
    Event listeners are now attached to the task and date text fields.
    
    Functions for the event listeners will need implementation.

--- to_do_list.js
+++ to_do_list.js
@@ -6,16 +6,41 @@ var list = $("#taskList");
 var id = 1;
 
 function addItem(){
-    let entry = document.createElement('li');
-    entry.appendChild(document.createTextNode("Example Task #" + id++ + " Due Date: 10/23/2023"));
-    let deleteButton = document.createElement("button");
+    let entry = document.createElement('li'); //New list entry for the new task.
+    let taskText = document.createElement('span'); //New Text span for the task portion.
+    let dateText = document.createElement('span'); //New Text span for the due date portion.
+    let deleteButton = document.createElement("button"); //New Button element for deleting the task.
+    
+    taskText.textContent = "Example Task #" + id; //Set dummy text (for now).
+    dateText.textContent = "Due Date: 10/02/2023"; //Set dummy text (for now).
+    deleteButton.textContent = "X"; //Button text
+    
+    taskText.setAttribute("id", "task" + id);
+    dateText.setAttribute("id", "date" + id);
+    
     //add a click event to remove the current item
     deleteButton.addEventListener("click", function() {
-      entry.remove()
+      entry.remove();
     });
-    deleteButton.textContent = "X";
+    
+    entry.appendChild(taskText);
+    entry.appendChild(dateText);
     entry.appendChild(deleteButton);
     list.appendChild(entry);
+
+    $("#task" + id).addEventListener("dblclick", task);
+    $("#date" + id).addEventListener("dblclick", date);
+    id += 1;
+}
+
+//Implement this function to be able to modify the task text.
+function task () {
+    console.log("TaskText");
+}
+
+//Implement this function to be able to modify the date text.
+function date () {
+    console.log("DateText");
 }
 
 document.addEventListener("DOMContentLoaded", () => {
